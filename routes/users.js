ba651f9bc74f26070199e51c6052759379da32cb
Rubin <rjhand@gmail.com>
2020-06-28 02:07:11 +0000

    Add functions that return queries

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
@@ -22,4 +22,44 @@ module.exports = (db) => {
       });
   });
   return router;
+};
+
+// MOVE THESE EVENTUALLY
+
+//Returns all the user's maps
+const getUserMaps = function (db, userId) {
+  return db.query(`
+    SELECT *
+    FROM maps
+    WHERE userId = $1
+    `, [userId]);
+};
+
+//Adds a new map to the database
+const addMap = function (db, userId, description) {
+  return db.query(`
+    INSERT INTO maps (userId, description)
+    VALUES ($1, $2)
+    RETURNING *`, [userId, description]);
+};
+
+//Favourite a map
+const favMap = function (db, userId, mapId) {
+  return db.query(`
+    INSERT INTO favourite_maps (userId, mapId)
+    VALUES ($1, $2)
+    RETURNING *`, [userId, mapId]);
+};
+
+//Returns maps not created by or favourited by user
+const notUserMaps = function (db, userId) {
+  return db.query(`
+    SELECT *
+    FROM maps WHERE NOT userId = $1
+      AND userId NOT IN (
+        SELECT *
+        FROM favourite_maps
+        WHERE NOT userId = $1
+      )
+  )`, [userId]);
 };
