0f3ea11f0c8eaaf82f24a9aca5200c0fd8d0c32a
Rubin <rjhand@gmail.com>
2020-06-28 02:48:46 +0000

    Add more functions for queries

--- routes/users.js
+++ routes/users.js
@@ -26,34 +26,63 @@ module.exports = (db) => {
 
 // MOVE THESE EVENTUALLY
 
+// const { db } = require('<ENTER PATH HERE BEFORE IMPLEMENTING>')
+
+//Returns maps with the most likes
+const getMostLikedMaps = function (db, mapId) {
+  return db(`
+    SELECT *
+    FROM maps
+    ORDER BY num_of_like DESC
+  `, [mapId])
+    .then(res => res.rows);
+};
+
 //Returns all the user's maps
 const getUserMaps = function (db, userId) {
-  return db.query(`
+  return db(`
     SELECT *
     FROM maps
     WHERE userId = $1
-    `, [userId]);
+    `, [userId])
+    .then(res => res.rows);
+};
+
+//Returns all the user favourites and created
+const getAllUserContributions = function (db, userId) {
+  return db(`
+  SELECT *
+  FROM maps WHERE userId = $1
+    AND userId IN (
+      SELECT *
+      FROM favourite_maps
+      WHERE userId = $1
+    )
+  )`, [userId])
+    .then(res => res.rows);
 };
 
 //Adds a new map to the database
 const addMap = function (db, userId, description) {
-  return db.query(`
+  return db(`
     INSERT INTO maps (userId, description)
     VALUES ($1, $2)
-    RETURNING *`, [userId, description]);
+    RETURNING *`, [userId, description])
+    .then(res => res.rows);
 };
 
 //Favourite a map
 const favMap = function (db, userId, mapId) {
-  return db.query(`
+  return db(`
     INSERT INTO favourite_maps (userId, mapId)
     VALUES ($1, $2)
-    RETURNING *`, [userId, mapId]);
+    RETURNING *`, [userId, mapId])
+    .then(res => res.rows);
 };
 
 //Returns maps not created by or favourited by user
 const notUserMaps = function (db, userId) {
-  return db.query(`
+  return db(`
     SELECT *
     FROM maps WHERE NOT userId = $1
       AND userId NOT IN (
@@ -61,5 +90,15 @@ const notUserMaps = function (db, userId) {
         FROM favourite_maps
         WHERE NOT userId = $1
       )
-  )`, [userId]);
+  )`, [userId])
+    .then(res => res.rows);
+};
+
+module.exports = {
+  getMostLikedMaps,
+  getAllUserContributions,
+  getUserMaps,
+  addMap,
+  favMap,
+  notUserMaps
 };
