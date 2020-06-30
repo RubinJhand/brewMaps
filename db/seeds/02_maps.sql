0e0a87ac2769074c4e8426e6b957aec938d9738c
Rubin <rjhand@gmail.com>
2020-06-30 19:47:23 +0000

    Make changes to database

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,3 +1,5 @@
-INSERT INTO maps (user_id, description, num_of_like)
+INSERT INTO maps (user_id, title, num_like)
 VALUES (1, 'new coffee houses', 3), 
-(2, 'fun coffee houses', 5);
+(1, 'fun coffee houses', 15),
+(1, 'fun coffee houses', 4435),
+(2, 'fun coffee houses', 3435);
