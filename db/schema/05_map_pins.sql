4782afc8cf04cb253a783f28f1da9dbc75a9d08a
Rubin <rjhand@gmail.com>
2020-07-01 03:22:10 +0000

    Remove files to reflect changes to database structure

--- db/schema/05_map_pins.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS map_pins CASCADE;
-
-CREATE TABLE map_pins (
-  -- id SERIAL PRIMARY KEY NOT NULL,
-  pin_id INTEGER REFERENCES pins(id),
-  map_id INTEGER REFERENCES maps(id)
-);
