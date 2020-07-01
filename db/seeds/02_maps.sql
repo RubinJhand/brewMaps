34368b5a1cfd6de0c4e07db251e519dc722f2beb
Rubin <rjhand@gmail.com>
2020-07-01 03:39:22 +0000

    Fix syntax errors

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -3,8 +3,8 @@ VALUES (1, 'Hipster Coffee', 3),
 (1, 'Bullet-Like Coffee', 15),
 (1, 'Timmy Ho The Master', 345),
 (2, 'Single Source Yum Yum', 64),
-(3, 'First St Coffee', 38)
-(3, 'Alligator Coffee List', 32006)
-(4, 'Local Coffees by Brenda', 87110)
-(4, 'Get Brews Here', 734186)
+(3, 'First St Coffee', 38),
+(3, 'Alligator Coffee List', 32006),
+(4, 'Local Coffees by Brenda', 87110),
+(4, 'Get Brews Here', 734186),
 (4, 'Lists of Places I Like', 8);
