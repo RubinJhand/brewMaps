6573df971c64843289f3093589711353e37fec74
Rubin <rjhand@gmail.com>
2020-07-01 14:48:07 +0000

    Add cookie-session dependeny

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require('cookie-session')
 const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require('morgan');
@@ -23,6 +24,10 @@ app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
 app.use("/styles", sass({
   src: __dirname + "/styles",
   dest: __dirname + "/public/styles",
@@ -79,9 +84,9 @@ app.get("/", (req, res) => {
   // res.render("index", templateVars);
 });
 
-app.get("login/:user", (req, res)=>{
+app.get("login/:user", (req, res) => {
   //set up cookie using req.params.user
-  
+
 
 })
 
